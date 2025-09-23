237699cb748e395b98d5937ef10fd11351cc27cc
ungaro <alp.guneysel@gmail.com>
2025-09-23 10:37:50 -0400

    add bsc <-> ethereum

---
+++ layerzero.plume.ethereum-bsc.config.ts
@@ -0,0 +1,47 @@
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
+import { generateConnectionsConfig } from '@layerzerolabs/metadata-tools'
+import type { OAppEnforcedOption, OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+
+// Existing PlumeOFT on Ethereum mainnet (address known in repo configs)
+const ethereumMainnetContract: OmniPointHardhat = {
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
+    address: '0xbDA8a2285F4C3e75b37E467C4DB9bC633FfbD29d',
+}
+
+// PlumeOFT to be deployed on BSC mainnet
+const bscMainnetContract: OmniPointHardhat = {
+    eid: EndpointId.BSC_V2_MAINNET,
+    contractName: 'PlumeOFT',
+}
+
+const ENFORCED: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 120000,
+        value: 0,
+    },
+]
+
+export default async function () {
+    const connections = await generateConnectionsConfig([
+        [
+            ethereumMainnetContract,
+            bscMainnetContract,
+            [['LayerZero Labs', 'Stargate'], []],
+            [5, 5],
+            [ENFORCED, ENFORCED],
+        ],
+    ])
+
+    return {
+        contracts: [
+            { contract: ethereumMainnetContract },
+            { contract: bscMainnetContract },
+        ],
+        connections,
+    }
+}
+
+
